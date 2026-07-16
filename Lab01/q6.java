c117aaf2de92b8a1a36aae86945d2785202ca094
marcuz02 <hirungunaratne8@gmail.com>
2026-07-16 12:46:12 +0530

    Create q6.java

---
+++ Lab01/q6.java
@@ -0,0 +1,11 @@
+public class q6 {
+    
+    public static void main(String[] args) {
+        int i;
+
+        for(i=0 ; i<=4 ; i++)
+        {
+            System.out.println("Executing Loop 0");
+        }
+    }
+}
