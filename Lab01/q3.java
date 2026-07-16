f5ca033559743c715eef59b321129093c57f27cf
marcuz02 <hirungunaratne8@gmail.com>
2026-07-16 12:46:20 +0530

    Create q3.java

---
+++ Lab01/q3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class q3 {
+    public static void main(String[] args) {
+        
+        //user input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a Value");
+        int num = sc.nextInt();
+
+        System.out.println("Value entered by the user:" +num);
+
+       sc.close(); 
+    }
+}
